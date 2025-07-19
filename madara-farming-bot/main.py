413d62bc3553ca34a8f05fb8d4a5450e904d6e2d
MadaraSenSei1 <omarkurd407@gmail.com>
2025-07-19 18:43:22 +0200

    Update main.py

--- madara-farming-bot/main.py
+++ madara-farming-bot/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from bot.travian_bot import get_farm_lists
