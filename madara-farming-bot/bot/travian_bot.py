37066ffb1a6f36f1bdb4cce7a2b610a7eb4ffbb7
MadaraSenSei1 <omarkurd407@gmail.com>
2025-07-19 18:12:07 +0200

    Update travian_bot.py

--- madara-farming-bot/bot/travian_bot.py
+++ madara-farming-bot/bot/travian_bot.py
@@ -14,8 +14,11 @@ def get_farm_lists(username, password, server_url, proxy_ip="", proxy_port="", p
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     if proxy_ip and proxy_port:
-        proxy_auth = f"{proxy_user}:{proxy_pass}@" if proxy_user and proxy_pass else ""
-        chrome_options.add_argument(f'--proxy-server=http://{proxy_auth}{proxy_ip}:{proxy_port}')
+    if proxy_user and proxy_pass:
+        proxy_auth = f"{proxy_user}:{proxy_pass}@{proxy_ip}:{proxy_port}"
+        chrome_options.add_argument(f"--proxy-server=http://{proxy_auth}")
+    else:
+        chrome_options.add_argument(f"--proxy-server=http://{proxy_ip}:{proxy_port}")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
